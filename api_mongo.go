138cb2a35060e703308465ff8001d286da9ddcce
李光春 <gc@dtapp.net>
2022-08-29 15:20:02 +0800

    - update mongo time

--- api_mongo.go
+++ api_mongo.go
@@ -8,14 +8,13 @@ import (
 	"go.dtapp.net/gourl"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"log"
-	"time"
 )
 
 // 模型结构体
 type apiMongolLog struct {
 	LogId                 primitive.ObjectID `json:"log_id,omitempty" bson:"_id,omitempty"`                                      //【记录】编号
 	TraceId               string             `json:"trace_id,omitempty" bson:"trace_id,omitempty"`                               //【系统】跟踪编号
-	RequestTime           time.Time          `json:"request_time,omitempty" bson:"request_time,omitempty"`                       //【请求】时间
+	RequestTime           primitive.DateTime `json:"request_time,omitempty" bson:"request_time,omitempty"`                       //【请求】时间
 	RequestUri            string             `json:"request_uri,omitempty" bson:"request_uri,omitempty"`                         //【请求】链接
 	RequestUrl            string             `json:"request_url,omitempty" bson:"request_url,omitempty"`                         //【请求】链接
 	RequestApi            string             `json:"request_api,omitempty" bson:"request_api,omitempty"`                         //【请求】接口
@@ -26,7 +25,7 @@ type apiMongolLog struct {
 	ResponseStatusCode    int                `json:"response_status_code,omitempty" bson:"response_status_code,omitempty"`       //【返回】状态码
 	ResponseBody          interface{}        `json:"response_body,omitempty" bson:"response_body,omitempty"`                     //【返回】内容
 	ResponseContentLength int64              `json:"response_content_length,omitempty" bson:"response_content_length,omitempty"` //【返回】大小
-	ResponseTime          time.Time          `json:"response_time,omitempty" bson:"response_time,omitempty"`                     //【返回】时间
+	ResponseTime          primitive.DateTime `json:"response_time,omitempty" bson:"response_time,omitempty"`                     //【返回】时间
 	SystemHostName        string             `json:"system_host_name,omitempty" bson:"system_host_name,omitempty"`               //【系统】主机名
 	SystemInsideIp        string             `json:"system_inside_ip,omitempty" bson:"system_inside_ip,omitempty"`               //【系统】内网ip
 	GoVersion             string             `json:"go_version,omitempty" bson:"go_version,omitempty"`                           //【程序】Go版本
@@ -59,19 +58,19 @@ func (c *ApiClient) MongoQuery() *dorm.MongoClient {
 // MongoMiddleware 中间件
 func (c *ApiClient) MongoMiddleware(ctx context.Context, request gorequest.Response, sdkVersion string) {
 	err := c.mongoRecord(ctx, apiMongolLog{
-		RequestTime:           request.RequestTime,                          //【请求】时间
-		RequestUri:            request.RequestUri,                           //【请求】链接
-		RequestUrl:            gourl.UriParse(request.RequestUri).Url,       //【请求】链接
-		RequestApi:            gourl.UriParse(request.RequestUri).Path,      //【请求】接口
-		RequestMethod:         request.RequestMethod,                        //【请求】方式
-		RequestParams:         request.RequestParams,                        //【请求】参数
-		RequestHeader:         request.RequestHeader,                        //【请求】头部
-		ResponseHeader:        request.ResponseHeader,                       //【返回】头部
-		ResponseStatusCode:    request.ResponseStatusCode,                   //【返回】状态码
-		ResponseBody:          dorm.JsonDecodeNoError(request.ResponseBody), //【返回】内容
-		ResponseContentLength: request.ResponseContentLength,                //【返回】大小
-		ResponseTime:          request.ResponseTime,                         //【返回】时间
-		SdkVersion:            sdkVersion,                                   //【程序】Sdk版本
+		RequestTime:           primitive.NewDateTimeFromTime(request.RequestTime),  //【请求】时间
+		RequestUri:            request.RequestUri,                                  //【请求】链接
+		RequestUrl:            gourl.UriParse(request.RequestUri).Url,              //【请求】链接
+		RequestApi:            gourl.UriParse(request.RequestUri).Path,             //【请求】接口
+		RequestMethod:         request.RequestMethod,                               //【请求】方式
+		RequestParams:         request.RequestParams,                               //【请求】参数
+		RequestHeader:         request.RequestHeader,                               //【请求】头部
+		ResponseHeader:        request.ResponseHeader,                              //【返回】头部
+		ResponseStatusCode:    request.ResponseStatusCode,                          //【返回】状态码
+		ResponseBody:          dorm.JsonDecodeNoError(request.ResponseBody),        //【返回】内容
+		ResponseContentLength: request.ResponseContentLength,                       //【返回】大小
+		ResponseTime:          primitive.NewDateTimeFromTime(request.ResponseTime), //【返回】时间
+		SdkVersion:            sdkVersion,                                          //【程序】Sdk版本
 	})
 	if err != nil {
 		if c.mongoConfig.debug {
@@ -83,19 +82,19 @@ func (c *ApiClient) MongoMiddleware(ctx context.Context, request gorequest.Respo
 // MongoMiddlewareXml 中间件
 func (c *ApiClient) MongoMiddlewareXml(ctx context.Context, request gorequest.Response, sdkVersion string) {
 	err := c.mongoRecord(ctx, apiMongolLog{
-		RequestTime:           request.RequestTime,                         //【请求】时间
-		RequestUri:            request.RequestUri,                          //【请求】链接
-		RequestUrl:            gourl.UriParse(request.RequestUri).Url,      //【请求】链接
-		RequestApi:            gourl.UriParse(request.RequestUri).Path,     //【请求】接口
-		RequestMethod:         request.RequestMethod,                       //【请求】方式
-		RequestParams:         request.RequestParams,                       //【请求】参数
-		RequestHeader:         request.RequestHeader,                       //【请求】头部
-		ResponseHeader:        request.ResponseHeader,                      //【返回】头部
-		ResponseStatusCode:    request.ResponseStatusCode,                  //【返回】状态码
-		ResponseBody:          dorm.XmlDecodeNoError(request.ResponseBody), //【返回】内容
-		ResponseContentLength: request.ResponseContentLength,               //【返回】大小
-		ResponseTime:          request.ResponseTime,                        //【返回】时间
-		SdkVersion:            sdkVersion,                                  //【程序】Sdk版本
+		RequestTime:           primitive.NewDateTimeFromTime(request.RequestTime),  //【请求】时间
+		RequestUri:            request.RequestUri,                                  //【请求】链接
+		RequestUrl:            gourl.UriParse(request.RequestUri).Url,              //【请求】链接
+		RequestApi:            gourl.UriParse(request.RequestUri).Path,             //【请求】接口
+		RequestMethod:         request.RequestMethod,                               //【请求】方式
+		RequestParams:         request.RequestParams,                               //【请求】参数
+		RequestHeader:         request.RequestHeader,                               //【请求】头部
+		ResponseHeader:        request.ResponseHeader,                              //【返回】头部
+		ResponseStatusCode:    request.ResponseStatusCode,                          //【返回】状态码
+		ResponseBody:          dorm.XmlDecodeNoError(request.ResponseBody),         //【返回】内容
+		ResponseContentLength: request.ResponseContentLength,                       //【返回】大小
+		ResponseTime:          primitive.NewDateTimeFromTime(request.ResponseTime), //【返回】时间
+		SdkVersion:            sdkVersion,                                          //【程序】Sdk版本
 	})
 	if err != nil {
 		if c.mongoConfig.debug {
@@ -107,19 +106,19 @@ func (c *ApiClient) MongoMiddlewareXml(ctx context.Context, request gorequest.Re
 // MongoMiddlewareCustom 中间件
 func (c *ApiClient) MongoMiddlewareCustom(ctx context.Context, api string, request gorequest.Response, sdkVersion string) {
 	err := c.mongoRecord(ctx, apiMongolLog{
-		RequestTime:           request.RequestTime,                          //【请求】时间
-		RequestUri:            request.RequestUri,                           //【请求】链接
-		RequestUrl:            gourl.UriParse(request.RequestUri).Url,       //【请求】链接
-		RequestApi:            api,                                          //【请求】接口
-		RequestMethod:         request.RequestMethod,                        //【请求】方式
-		RequestParams:         request.RequestParams,                        //【请求】参数
-		RequestHeader:         request.RequestHeader,                        //【请求】头部
-		ResponseHeader:        request.ResponseHeader,                       //【返回】头部
-		ResponseStatusCode:    request.ResponseStatusCode,                   //【返回】状态码
-		ResponseBody:          dorm.JsonDecodeNoError(request.ResponseBody), //【返回】内容
-		ResponseContentLength: request.ResponseContentLength,                //【返回】大小
-		ResponseTime:          request.ResponseTime,                         //【返回】时间
-		SdkVersion:            sdkVersion,                                   //【程序】Sdk版本
+		RequestTime:           primitive.NewDateTimeFromTime(request.RequestTime),  //【请求】时间
+		RequestUri:            request.RequestUri,                                  //【请求】链接
+		RequestUrl:            gourl.UriParse(request.RequestUri).Url,              //【请求】链接
+		RequestApi:            api,                                                 //【请求】接口
+		RequestMethod:         request.RequestMethod,                               //【请求】方式
+		RequestParams:         request.RequestParams,                               //【请求】参数
+		RequestHeader:         request.RequestHeader,                               //【请求】头部
+		ResponseHeader:        request.ResponseHeader,                              //【返回】头部
+		ResponseStatusCode:    request.ResponseStatusCode,                          //【返回】状态码
+		ResponseBody:          dorm.JsonDecodeNoError(request.ResponseBody),        //【返回】内容
+		ResponseContentLength: request.ResponseContentLength,                       //【返回】大小
+		ResponseTime:          primitive.NewDateTimeFromTime(request.ResponseTime), //【返回】时间
+		SdkVersion:            sdkVersion,                                          //【程序】Sdk版本
 	})
 	if err != nil {
 		if c.mongoConfig.debug {
--- gin_mongo.go
+++ gin_mongo.go
@@ -14,14 +14,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
-	"time"
 )
 
 // 模型结构体
 type ginMongoLog struct {
 	LogId             primitive.ObjectID `json:"log_id,omitempty" bson:"_id,omitempty"`                              //【记录】编号
 	TraceId           string             `json:"trace_id,omitempty" bson:"trace_id,omitempty"`                       //【系统】跟踪编号
-	RequestTime       time.Time          `json:"request_time,omitempty" bson:"request_time,omitempty"`               //【请求】时间
+	RequestTime       primitive.DateTime `json:"request_time,omitempty" bson:"request_time,omitempty"`               //【请求】时间
 	RequestUri        string             `json:"request_uri,omitempty" bson:"request_uri,omitempty"`                 //【请求】请求链接 域名+路径+参数
 	RequestUrl        string             `json:"request_url,omitempty" bson:"request_url,omitempty"`                 //【请求】请求链接 域名+路径
 	RequestApi        string             `json:"request_api,omitempty" bson:"request_api,omitempty"`                 //【请求】请求接口 路径
@@ -38,7 +37,7 @@ type ginMongoLog struct {
 	RequestIpCity     string             `json:"request_ip_city,omitempty" bson:"request_ip_city,omitempty"`         //【请求】请求客户端城市
 	RequestIpIsp      string             `json:"request_ip_isp,omitempty" bson:"request_ip_isp,omitempty"`           //【请求】请求客户端运营商
 	RequestHeader     interface{}        `json:"request_header,omitempty" bson:"request_header,omitempty"`           //【请求】请求头
-	ResponseTime      time.Time          `json:"response_time,omitempty" bson:"response_time,omitempty"`             //【返回】时间
+	ResponseTime      primitive.DateTime `json:"response_time,omitempty" bson:"response_time,omitempty"`             //【返回】时间
 	ResponseCode      int                `json:"response_code,omitempty" bson:"response_code,omitempty"`             //【返回】状态码
 	ResponseMsg       string             `json:"response_msg,omitempty" bson:"response_msg,omitempty"`               //【返回】描述
 	ResponseData      interface{}        `json:"response_data,omitempty" bson:"response_data,omitempty"`             //【返回】数据
@@ -175,7 +174,7 @@ func (c *GinClient) MongoMiddleware() gin.HandlerFunc {
 					}
 					err := c.mongoRecord(ginMongoLog{
 						TraceId:           traceId,                                                      //【系统】跟踪编号
-						RequestTime:       requestTime,                                                  //【请求】时间
+						RequestTime:       primitive.NewDateTimeFromTime(requestTime),                   //【请求】时间
 						RequestUri:        host + ginCtx.Request.RequestURI,                             //【请求】请求链接
 						RequestUrl:        ginCtx.Request.RequestURI,                                    //【请求】请求链接
 						RequestApi:        gourl.UriFilterExcludeQueryString(ginCtx.Request.RequestURI), //【请求】请求接口
@@ -192,7 +191,7 @@ func (c *GinClient) MongoMiddleware() gin.HandlerFunc {
 						RequestIpCity:     requestClientIpCity,                                          //【请求】请求客户端城市
 						RequestIpIsp:      requestClientIpIsp,                                           //【请求】请求客户端运营商
 						RequestHeader:     ginCtx.Request.Header,                                        //【请求】请求头
-						ResponseTime:      gotime.Current().Time,                                        //【返回】时间
+						ResponseTime:      primitive.NewDateTimeFromTime(gotime.Current().Time),         //【返回】时间
 						ResponseCode:      responseCode,                                                 //【返回】状态码
 						ResponseData:      c.jsonUnmarshal(responseBody),                                //【返回】数据
 						CostTime:          endTime - startTime,                                          //【系统】花费时间
@@ -208,7 +207,7 @@ func (c *GinClient) MongoMiddleware() gin.HandlerFunc {
 					}
 					err := c.mongoRecord(ginMongoLog{
 						TraceId:           traceId,                                                      //【系统】跟踪编号
-						RequestTime:       requestTime,                                                  //【请求】时间
+						RequestTime:       primitive.NewDateTimeFromTime(requestTime),                   //【请求】时间
 						RequestUri:        host + ginCtx.Request.RequestURI,                             //【请求】请求链接
 						RequestUrl:        ginCtx.Request.RequestURI,                                    //【请求】请求链接
 						RequestApi:        gourl.UriFilterExcludeQueryString(ginCtx.Request.RequestURI), //【请求】请求接口
@@ -225,7 +224,7 @@ func (c *GinClient) MongoMiddleware() gin.HandlerFunc {
 						RequestIpCity:     requestClientIpCity,                                          //【请求】请求客户端城市
 						RequestIpIsp:      requestClientIpIsp,                                           //【请求】请求客户端运营商
 						RequestHeader:     ginCtx.Request.Header,                                        //【请求】请求头
-						ResponseTime:      gotime.Current().Time,                                        //【返回】时间
+						ResponseTime:      primitive.NewDateTimeFromTime(gotime.Current().Time),         //【返回】时间
 						ResponseCode:      responseCode,                                                 //【返回】状态码
 						ResponseData:      c.jsonUnmarshal(responseBody),                                //【返回】数据
 						CostTime:          endTime - startTime,                                          //【系统】花费时间
