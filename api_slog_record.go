9d9cd5fe7ccd24c33ed6c10704574fadbbe6d283
dtapps <li.gc@icloud.com>
2024-01-16 15:59:07 +0800

    - update `slog`

--- api_slog_record.go
+++ api_slog_record.go
@@ -6,24 +6,7 @@ import (
 	"go.dtapp.net/gorequest"
 	"go.dtapp.net/gotrace_id"
 	"go.dtapp.net/gourl"
-	"time"
 )
-
-// 结构体
-type apiSLog struct {
-	TraceID            string                 `json:"trace_id,omitempty"`
-	RequestTime        time.Time              `json:"request_time,omitempty"`
-	RequestUri         string                 `json:"request_uri,omitempty"`
-	RequestUrl         string                 `json:"request_url,omitempty"`
-	RequestApi         string                 `json:"request_api,omitempty"`
-	RequestMethod      string                 `json:"request_method,omitempty"`
-	RequestParams      map[string]interface{} `json:"request_params,omitempty"`
-	RequestHeader      map[string]string      `json:"request_header,omitempty"`
-	ResponseHeader     map[string][]string    `json:"response_header,omitempty"`
-	ResponseStatusCode int                    `json:"response_status_code,omitempty"`
-	ResponseBody       map[string]interface{} `json:"response_body,omitempty"`
-	ResponseTime       time.Time              `json:"response_time,omitempty,omitempty"`
-}
 
 // Middleware 中间件
 func (al *ApiSLog) Middleware(ctx context.Context, request gorequest.Response) {
